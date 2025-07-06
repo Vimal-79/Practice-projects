0579ef46187a543a8fc44e8e4a55ac2ee0347006
Vimal <vimalkumar200713@gmail.com>
2025-07-06 14:58:02 +0530

    Update App.jsx

--- password-manager/src/App.jsx
+++ password-manager/src/App.jsx
@@ -1,14 +1,19 @@
 import Navbar from "./components/Navbar"
 import Manager from "./components/manager"
 import Hero from "./components/Hero"
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Hero />
+      <div className="flex flex-col items-center justify-center max-[500px]:my-4 my-8 max-[500px]:text-[10px]">
+        <Hero />
+        <p className="text-gray-600">Your own password manager</p>
+      </div>
       <Manager />
+      <Footer />
     </>
   )
 }
