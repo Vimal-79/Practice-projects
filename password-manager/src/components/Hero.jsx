7f7fd6334abda92f7ec3eafd075ed2a852bc9552
Vimal <vimalkumar200713@gmail.com>
2025-07-06 14:27:06 +0530

    Update Hero.jsx

--- password-manager/src/components/Hero.jsx
+++ password-manager/src/components/Hero.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import Navbar from "./components/Navbar"
+import Manager from "./components/manager"
+import Hero from "./components/Hero"
 
-function Hero() {
+function App() {
+
   return (
-    <div className="flex flex-col items-center justify-center max-[500px]:my-4 my-8 max-[500px]:text-[10px]">
-        <div className="flex font-extrabold capitalize text-3xl max-[500px]:text-[18px]">
-          <span className="text-green-500">&lt;</span>
-          pass
-          <span className="text-green-500 uppercase">/op&gt;</span>
-        </div>
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center max-[500px]:my-4 my-8 max-[500px]:text-[10px]">
+        <Hero />
         <p className="text-gray-600">Your own password manager</p>
       </div>
+      <Manager />
+    </>
   )
 }
 
-export default Hero
+export default App
