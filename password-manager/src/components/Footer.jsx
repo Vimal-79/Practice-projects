e73784e7f97fd2c9f5bf0e23a60e7088ea60cdc0
Vimal <vimalkumar200713@gmail.com>
2025-07-06 14:57:27 +0530

    Update Footer.jsx

--- password-manager/src/components/Footer.jsx
+++ password-manager/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Hero from './Hero'
 
 function Footer() {
   return (
-    <div className='bg-neutral-800 flex flex-col items-center fixed w-full bottom-0'>
+    <div className='bg-neutral-800 flex flex-col items-center fixed w-full bottom-0 gap-1'>
       <Hero />
       <p className='flex text-white'>Made with <img className='mx-2 w-5 h-5' src="./src/assets/heart.png" alt="heart image" /> by Vimal</p>
     </div>
