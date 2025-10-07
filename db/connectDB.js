bdf384488645dbd9a430a6523b5b6cb0afa9e3bb
Vimal-79 <vimalkumar200713@gmail.com>
2025-10-07 12:36:52 +0530

    leak fixed

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,15 +1,3 @@
-// "use server"
-// import mongoose from 'mongoose';
-
-// const connectDB = async () => {
-
-// await mongoose.connect("mongodb://localhost:27017/MakeMeToCode")
-
-// console.log("connected to mongoDB")
-// console.log("mongodb://localhost:27017/MakeMeToCode")
-// }
-
-// export default connectDB;
 
 "use server"
 import mongoose from 'mongoose';
@@ -17,7 +5,6 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/MakeMeToCode');
-    // await mongoose.connect('mongodb+srv://vimalkumar200713:ri0wXA6pisW3fXrP@cluster.x5vqvcz.mongodb.net/');
     console.log('MongoDB connected successfully!');
   } catch (error) {
     console.error('MongoDB connection error:', error);
